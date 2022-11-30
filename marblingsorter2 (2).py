eb86b2f7d674e90dc73dbc7e48f049f0448674f2
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-11-30 22:08:21 +0530

    Add files via upload

---
+++ marblingsorter2 (2).py
@@ -0,0 +1,58 @@
+import streamlit as st
+import tensorflow as tf
+import streamlit as st
+
+
+@st.cache
+def load_model():
+
+    save_dest = Path('model')
+    save_dest.mkdir(exist_ok=True)
+    
+    f_checkpoint = Path("model/content/drive/MyDrive/code/model_save/my_model1.hdf5")
+
+    if not f_checkpoint.exists():
+        with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
+            from GD_download import download_file_from_google_drive
+            download_file_from_google_drive(cloud_model_location, f_checkpoint)
+    
+    model = torch.load(f_checkpoint, map_location=device)
+    model.eval()
+    return model
+
+st.write("""
+         # Beef Classification
+         """
+         )
+
+file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+st.set_option('deprecation.showfileUploaderEncoding', False)
+def import_and_predict(image_data, model):
+    
+        size = (180,180)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        
+        img_reshape = img[np.newaxis,...]
+    
+        predictions = model.predict(img_reshape)
+        
+        return predictions
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    predictions = import_and_predict(image, model)
+    score = tf.nn.softmax(predictions[0])
+    st.write(predictions)
+    st.write(score)
+    print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
